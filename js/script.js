b36f12bb71cff33d89287924bfbce1e1a49a49aa
Il tuo nome <la_tua_email@example.com>
2024-05-16 15:55:17 +0200

    added prompts

--- js/script.js
+++ js/script.js
@@ -1,18 +1,9 @@
-// // > Scrivi una funzione che dato un numero inverta le sue cifre: 1234 -> 4321
+// Palidroma
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
+
+let primaParola = prompt("Inserisci la prima parola");
+let secondaParola = prompt("Inserisci la seconda parola");
 
 
-// function getReverse(numero) {
 
-//     let numeroStringa = numero.toString();
-//     let invertito = numeroStringa.split("").reverse().join("");
-
-//     let numeroInvertito = parseInt(invertito, 10)
-
-//     return numeroInvertito;
-//   }
-
-//   let numeroUtente = prompt("Inserisci un numero:");
- 
-//   let numeroInvertito = getReverse(numero);
-
-//   console.log(numeroInvertito);
