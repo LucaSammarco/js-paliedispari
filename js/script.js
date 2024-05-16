93f1476f14e66443a856cad0c00855a337c6cd68
Il tuo nome <la_tua_email@example.com>
2024-05-16 16:09:38 +0200

    trying to create a loop to invert the letters

--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
 // Palidroma
-// Chiedere all’utente di inserire una parola
-// Creare una funzione per capire se la parola inserita è palindroma
+// // Chiedere all’utente di inserire una parola
+// // Creare una funzione per capire se la parola inserita è palindroma
 
-let primaParola = prompt("Inserisci la prima parola");
-let secondaParola = prompt("Inserisci la seconda parola");
-
+// let parola = prompt("Inserisci la prima parola");
 
 
+function palindromChecker(word) {
+
+    let parolaInvertita ="";
+
+
+    for (let index = word.length -1; index >= 0; index++) {
+      
+      parolaInvertita += word[];
+      
+      
+    }
+
+}
